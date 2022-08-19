470548dd32b6be877494be215a3b68b6c0b18369
Waleed Amjad <waleedamjad99@hotmail.com>
2022-08-19 23:34:50 +0500

    Fix linter errors

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,24 +1,24 @@
 module MyEnumerable
-    def all?
-      each do |el|
-        return false unless yield(el)
-      end
-      true
-    end
   
-    def any?
-      each do |el|
-        return true if yield(el)
-      end
-      false
+  def all?
+    each do |el|
+      return false unless yield(el)
     end
-  
-    def filter
-      array = []
-      each do |el|
-        array << el if yield(el)
-      end
-      array
-    end
+    true
   end
-  
+
+  def any?
+    each do |el|
+      return true if yield(el)
+    end
+    false
+  end
+
+  def filter
+    array = []
+    each do |el|
+      array << el if yield(el)
+    end
+    array
+  end
+end
